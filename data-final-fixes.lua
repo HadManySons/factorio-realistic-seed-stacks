f01a2591b2afb4ef499e36c92970c87b04ccf572
CyberAustin <austin.y.schibler@gmail.com>
2025-07-23 15:50:47 +0200

    version 1.0

---
+++ data-final-fixes.lua
@@ -0,0 +1,7 @@
+data.raw["item"]["tree-seed"].stack_size = 1000
+data.raw["item"]["yumako-seed"].stack_size = 1000
+data.raw["item"]["jellynut-seed"].stack_size = 1000
+
+data.raw["item"]["tree-seed"].weight = 0.1 * kg
+data.raw["item"]["yumako-seed"].weight = 0.1 * kg
+data.raw["item"]["jellynut-seed"].weight = 0.1 * kg
